549029c50c17b475482dd8191e81a68d45ea8d20
JoleneKearse <pharaohnutz@gmail.com>
2024-09-16 17:46:57 -0400

    chore: convert to button

--- src/components/Action.tsx
+++ src/components/Action.tsx
@@ -5,10 +5,10 @@ type ActionProps = {
 
 export const Action = ({ img, className }: ActionProps) => {
   return (
-    <div className={`w-[130px] aspect-square rounded-full grid place-items-center  ${className ? className: ""}`}>
+    <button className={`w-[130px] aspect-square rounded-full grid place-items-center  ${className ? className: ""}`}>
       <div className="bg-neutral-200 w-[105px] aspect-square rounded-full grid place-items-center shadow-[0_-3px_2px_3px_#DADADA] mt-2">
         <img src={img} alt={img} />
       </div>
-    </div>
+    </button>
   );
 };
